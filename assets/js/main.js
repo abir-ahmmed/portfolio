648c17cf90bd6265652b9fc32e9698f6e0a0fb3d
abir-ahmmed <abirahmmedrana@outlook.com>
2023-07-04 22:04:44 +0600

    slider almost

--- assets/js/main.js
+++ assets/js/main.js
@@ -131,3 +131,42 @@ $(window).on("load resize",function() {
       count(0, counter[j], j);
     }
   });
+
+  // Slider
+  $('.testimonial-2-slider').slick({
+    dots: false,
+    infinite: false,
+    speed: 300,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+      // You can unslick at a given breakpoint now by adding:
+      // settings: "unslick"
+      // instead of a settings object
+    ]
+  });
