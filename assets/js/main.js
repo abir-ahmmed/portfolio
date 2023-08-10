f630bd33a49d55640dad324d9bb11f1eedcd4885
abir-ahmmed <abirahmmedrana@outlook.com>
2023-08-11 04:34:30 +0600

    color

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,3 +3,26 @@ function activeMenu(){
     var a = document.getElementById('nav_link');
     a.classList.add('active-menu');
 }
+
+console.log(document.images);
+
+const getThColor = ()=>{
+    // Hex
+    const randomNumber = Math.floor(Math.random()*16777215);
+    const ramdomCode = "#" +randomNumber.toString(16);
+    document.body.style.background=ramdomCode;
+    document.getElementById("introShape").style.borderColor = ramdomCode;
+
+	// document.getElementById("colorThcode").innerText = ramdomCode;
+	navigator.clipboard.writeText(ramdomCode)
+  }
+
+//   Event call
+  document.getElementById("cCBtn").addEventListener(
+    "click",
+    getThColor
+  )
+
+//   init call
+getThColor();
+
