b0028834adc5c742391da2c9890a57ce26ec486c
abir-ahmmed <abirahmmedrana@outlook.com>
2023-08-26 02:27:09 +0600

    responsive

---
+++ assets/js/main.js
@@ -0,0 +1,6 @@
+function navToggle() {
+    document.getElementById("toggleMenu").style.display = "block";
+  }
+function navClose() {
+    document.getElementById("toggleMenu").style.display = "none";
+  }
